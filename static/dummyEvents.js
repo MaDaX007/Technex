ffac352d3838c3c981a87779a8b5454526b35a8d
Geetesh Gedam <73989487+MaDaX007@users.noreply.github.com>
2022-03-29 21:15:33 +0530

    Add files via upload

---
+++ static/dummyEvents.js
@@ -0,0 +1,26 @@
+export const dummyEvents = [
+  {
+    price: 17,
+    from: 'Vikey',
+    to: '0xDavid',
+    date: '1 day ago',
+  },
+  {
+    price: 17,
+    from: 'Aditi',
+    to: '0xDavid',
+    date: '1 day ago',
+  },
+  {
+    price: 17,
+    from: 'Sneha',
+    to: '0xDavid',
+    date: '1 day ago',
+  },
+  {
+    price: 17,
+    from: 'david',
+    to: '0xDavid',
+    date: '1 day ago',
+  },
+]
